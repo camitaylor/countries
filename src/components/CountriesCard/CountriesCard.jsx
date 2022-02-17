70301fca0cff5a8df062b58646ad3613fa60230a
Cami Taylor <73480669+camitaylor@users.noreply.github.com>
2022-02-17 14:21:49 -0600

    Create CountriesCard and countriesListData.js

--- src/components/CountriesCard/CountriesCard.jsx
+++ src/components/CountriesCard/CountriesCard.jsx
@@ -1,29 +1,53 @@
-import React from 'react';
-import { Box, Card, CardHeader, CardBody, Image, Text } from 'grommet';
+import React, { useContext } from 'react';
+import { 
+  Box, 
+  Card, 
+  CardHeader, 
+  CardBody, 
+  Grid, 
+  Image, 
+  ResponsiveContext, 
+  Text } from 'grommet';
+import countriesListData from '../CountriesList/countriesListData';
 
-const CountriesCard = () => {
-  return (
-    <Card  height="small" width="small" background="light-1">
-      <CardHeader>
-        <Box height="xsmall" width="small">
-          <Image
-            fit="cover"
-            src="https://www.90daykorean.com/wp-content/uploads/2020/07/Countries-of-the-World.jpg"
-          />
-        </Box>
-      </CardHeader>
-      <CardBody pad="small">
-        <Box pad={{ bottom: 'xsmall'}}>
-          <Text size='small' weight='bold'>United States</Text>
-        </Box>
-        <Box>
-          <Text size='xsmall'>Region: North America</Text>
-          <Text size='xsmall'>Population: A LOT</Text>
-          <Text size='xsmall'>Capital: Washington DC</Text>
-        </Box>
-      </CardBody>
-    </Card>
-  )
-}
+
+const CountriesCard =  () => {
+  const size = useContext(ResponsiveContext);
+
+  const countriesArray = countriesListData.map(({id, name, region, population, capital}) => (
+        <Grid key={id} columns={size !== 'small' ? 'small' : '100%'} gap="medium">
+          <Card  
+            elevation="small"
+            height="small" 
+            width="small" 
+            background="light-1">
+            <CardHeader>
+              <Box height="xsmall" width="small">
+                <Image
+                  fit="cover"
+                  src="https://www.90daykorean.com/wp-content/uploads/2020/07/Countries-of-the-World.jpg"
+                />
+              </Box>
+            </CardHeader>
+            <CardBody pad="small">
+              <Box pad={{ bottom: 'xsmall'}}>
+                <Text size='small' weight='bold'>{name}</Text>
+              </Box>
+              <Box>
+                <Text size='xsmall'>Region: {region}</Text>
+                <Text size='xsmall'>Population: {population}</Text>
+                <Text size='xsmall'>Capital: {capital}</Text>
+              </Box>
+            </CardBody>
+          </Card>
+        </Grid>
+    ))
+  
+    return (
+      <Box>
+        {countriesArray}
+      </Box>
+    );
+  };
 
 export default CountriesCard;
